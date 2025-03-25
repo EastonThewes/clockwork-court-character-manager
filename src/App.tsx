3e7586ca53dacdd6389e6d49b54b927e0d21ad9b
Easton <easton.thewes@eagleresearchcorp.com>
2025-03-25 11:50:28 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import * as React from 'react';
-import Button from '@mui/joy/Button';
 import CharacterSheet from './components/CharacterSheet';
 import theme from "./theme"; // Import your custom theme
 import { ThemeProvider } from '@emotion/react';
-
 
 function App() {
   return (
